5f9f375e1b768f978bf40a56e589023e8b86f750
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 23:49:35 +0100

    Added BaseArea() test for correct area calculation

--- Sec-5-task-3-Area-And-Volume-of-a-box/utest.cpp
+++ Sec-5-task-3-Area-And-Volume-of-a-box/utest.cpp
@@ -17,3 +17,10 @@ TEST(BaseAreaTest, ThrowsInvalidArgumentWhenNegativeArgumentPassed)
     EXPECT_THROW(BaseArea(0.0, -1.0), std::invalid_argument);
     EXPECT_THROW(BaseArea(-1.0, -2.3), std::invalid_argument);
 }
+
+TEST(BaseAreaTest, CorrectlyCalculatesBaseAreaForPositiveArgs)
+{
+    EXPECT_DOUBLE_EQ(BaseArea(2.3, 4.4), 10.12);
+    EXPECT_DOUBLE_EQ(BaseArea(0.15, 12.1), 1.815);
+    EXPECT_DOUBLE_EQ(BaseArea(3.333, 2.11), 7.03263);
+}
