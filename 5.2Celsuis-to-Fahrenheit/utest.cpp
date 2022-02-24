d91b6e878fb819dd141d6d161801937d8433b6ff
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 12:12:09 +0100

    Added test for calculating negative values - GREEN

--- 5.2Celsuis-to-Fahrenheit/utest.cpp
+++ 5.2Celsuis-to-Fahrenheit/utest.cpp
@@ -1,3 +1,4 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "CelsiusToFahrenheit.hpp"
@@ -5,4 +6,11 @@
 TEST(CelsiusToFahrenheitTest, CalculatesCorrectValueFor0degCelsius)
 {
     EXPECT_DOUBLE_EQ(CelsiusToFahrenheit(0.0), 32.0);
+}
+
+TEST(CelsiusToFahrenheitTest, CalculatesNegativeCelsiusValuesCorrectly)
+{
+    EXPECT_DOUBLE_EQ(CelsiusToFahrenheit(-10.5), 13.1);
+    EXPECT_DOUBLE_EQ(CelsiusToFahrenheit(-144.75), -228.55);
+    EXPECT_THAT(CelsiusToFahrenheit(-51.0), testing::DoubleEq(-59.8));
 }
