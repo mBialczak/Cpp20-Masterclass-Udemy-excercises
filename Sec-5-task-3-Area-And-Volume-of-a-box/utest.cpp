f1ab49447f44cdc7e6284ce88a3f785bca928296
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 23:41:16 +0100

    Added BaseArea() test for throws when negative args

--- Sec-5-task-3-Area-And-Volume-of-a-box/utest.cpp
+++ Sec-5-task-3-Area-And-Volume-of-a-box/utest.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <exception>
+
 #include "box-calcs.hpp"
 
 TEST(BaseAreaTest, ReturnsZeroWhenAnyOfArgsIsZero)
@@ -7,4 +9,11 @@ TEST(BaseAreaTest, ReturnsZeroWhenAnyOfArgsIsZero)
     EXPECT_EQ(BaseArea(0, 2.0), 0);
     EXPECT_EQ(BaseArea(4.0, 0), 0);
     EXPECT_EQ(BaseArea(0, 0), 0);
+}
+
+TEST(BaseAreaTest, ThrowsInvalidArgumentWhenNegativeArgumentPassed)
+{
+    EXPECT_THROW(BaseArea(-1.0, 0), std::invalid_argument);
+    EXPECT_THROW(BaseArea(0.0, -1.0), std::invalid_argument);
+    EXPECT_THROW(BaseArea(-1.0, -2.3), std::invalid_argument);
 }
