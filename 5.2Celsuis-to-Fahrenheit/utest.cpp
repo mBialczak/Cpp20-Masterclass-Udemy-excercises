3321af813ba729989ddc7dde3a1353de208a3c51
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 12:26:09 +0100

    Added test calculating positive values - GREEN

--- 5.2Celsuis-to-Fahrenheit/utest.cpp
+++ 5.2Celsuis-to-Fahrenheit/utest.cpp
@@ -14,3 +14,11 @@ TEST(CelsiusToFahrenheitTest, CalculatesNegativeCelsiusValuesCorrectly)
     EXPECT_DOUBLE_EQ(CelsiusToFahrenheit(-144.75), -228.55);
     EXPECT_THAT(CelsiusToFahrenheit(-51.0), testing::DoubleEq(-59.8));
 }
+
+TEST(CelsiusToFahrenheitTest, CalculatesPositiveCelsiusValuesCorrectly)
+{
+    constexpr double precission = 0.001;
+    EXPECT_DOUBLE_EQ(CelsiusToFahrenheit(0.13), 32.234);
+    EXPECT_NEAR(CelsiusToFahrenheit(8.4), 47.12, precission);
+    EXPECT_THAT(CelsiusToFahrenheit(48.3), testing::DoubleNear(118.94, precission));
+}
