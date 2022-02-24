5da620f4eac205da07082c46420291b9872627f7
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 11:54:14 +0100

    Added everything up to first file running

---
+++ 5.2Celsuis-to-Fahrenheit/CelsiusToFahrenheit.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+inline double CelsiusToFahrenheit(double degrees_celsius)
+{
+    return (9.0 / 5.0) * degrees_celsius + 32;
+}
--- 5.2Celsuis-to-Fahrenheit/utest.cpp
+++ 5.2Celsuis-to-Fahrenheit/utest.cpp
@@ -1,1 +1,8 @@
 #include <gtest/gtest.h>
+
+#include "CelsiusToFahrenheit.hpp"
+
+TEST(CelsiusToFahrenheitTest, CalculatesCorrectValueFor0degCelsius)
+{
+    EXPECT_DOUBLE_EQ(CelsiusToFahrenheit(0.0), 32.0);
+}
