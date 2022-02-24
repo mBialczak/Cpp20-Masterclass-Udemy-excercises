f249bbc6a467968a5453817c98c27bcb2ec80b15
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 22:29:54 +0100

    Implemented main and added GetInputInCelsius

--- 5.2Celsuis-to-Fahrenheit/main.cpp
+++ 5.2Celsuis-to-Fahrenheit/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
+#include "CelsiusToFahrenheit.hpp"
+
+double GetInputInCelsiusDegrees()
+{
+    std::cout << "Enter value in Celsius degrees to be converted to Fahrenheit: ";
+    double celsius_degrees { -271.0 };
+    std::string input_line;
+    std::getline(std::cin, input_line);
+    std::istringstream stream(input_line);
+    if (!(stream >> celsius_degrees)) {
+        throw std::invalid_argument { "erroneous input!" };
+    }
+
+    return celsius_degrees;
+}
 
 int main()
 {
+    try {
+        double celsius_deg = GetInputInCelsiusDegrees();
+        std::cout << celsius_deg
+                  << " degrees Celsius is "
+                  << CelsiusToFahrenheit(celsius_deg)
+                  << " degrees Fahrenheit"
+                  << std::endl;
+    }
+    catch (const std::invalid_argument& e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
