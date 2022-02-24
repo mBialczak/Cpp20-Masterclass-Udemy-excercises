b8cc1b0d42686fbddc35c70f3fd58feea4d0e8a4
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 23:42:07 +0100

    Extended BaseArea() implementation
    BaseArea() throws when any argument passed is negative

--- Sec-5-task-3-Area-And-Volume-of-a-box/box-calcs.hpp
+++ Sec-5-task-3-Area-And-Volume-of-a-box/box-calcs.hpp
@@ -1,4 +1,10 @@
-double BaseArea(double width, double length)
+#include <exception>
+
+constexpr double BaseArea(double width, double length)
 {
+    if (width < 0.0 || length < 0.0) {
+        throw std::invalid_argument("Negative dimmension passed");
+    }
+
     return width * length;
 }
