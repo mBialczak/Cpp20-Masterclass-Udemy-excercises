2cfd344e8f9d246f65814820b514e3dbe3235a29
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-24 23:31:47 +0100

    Added BaseArea() test for 0 arguments

--- Sec-5-task-3-Area-And-Volume-of-a-box/utest.cpp
+++ Sec-5-task-3-Area-And-Volume-of-a-box/utest.cpp
@@ -1,1 +1,10 @@
 #include <gtest/gtest.h>
+
+#include "box-calcs.hpp"
+
+TEST(BaseAreaTest, ReturnsZeroWhenAnyOfArgsIsZero)
+{
+    EXPECT_EQ(BaseArea(0, 2.0), 0);
+    EXPECT_EQ(BaseArea(4.0, 0), 0);
+    EXPECT_EQ(BaseArea(0, 0), 0);
+}
