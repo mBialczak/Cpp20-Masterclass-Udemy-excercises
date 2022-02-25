5f6ef79354d94eb48ee4ffe2a52ff817524d7ff6
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-25 01:05:31 +0100

    Main function implemented

--- Sec-5-task-3-Area-And-Volume-of-a-box/main.cpp
+++ Sec-5-task-3-Area-And-Volume-of-a-box/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "box-calcs.hpp"
 
+void Prompt(const std::string& message)
+{
+    std::cout << message;
+}
+
+double GetDimmension()
+{
+    std::string line;
+    std::getline(std::cin, line);
+    double dimmension {};
+    std::istringstream stream(line);
+    if (!(stream >> dimmension)) {
+        throw std::invalid_argument { "Erroneus input!" };
+    }
+
+    return dimmension;
+}
 int main()
 {
+    try {
+        std::cout << "Welcome to box calculator. "
+                  << "Please type in width, length and height information:\n";
+        Prompt("width: ");
+        double width {};
+        width = GetDimmension();
+        Prompt("length: ");
+        double length {};
+        length = GetDimmension();
+        Prompt("height: ");
+        double height {};
+        height = GetDimmension();
+
+        std::cout << "The base area is: " << BaseArea(width, length) << '\n'
+                  << "The box volume is: " << BoxVolume(width, length, height) << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
